dda1d47543534b64be5e50a1b02c2a2a89624ca7
unknownsaying <15368104568@189.cn>
2026-01-03 18:00:00 +0800

    Update color.vb

--- claude/color.vb
+++ claude/color.vb
@@ -131,7 +131,7 @@ Imports System
 Imports System.Collections.Generic
 
 Module Red
-Public Interface IC1H4
+Interface IC1H4
     ReadOnly Property CarbonCount As Integer
     ReadOnly Property HydrogenCount As Integer
 End Interface
@@ -139,11 +139,11 @@ End Interface
 Class C1H4
     Implements IC1H4
 
-    Public ReadOnly Property CarbonCount As Integer Implements IC1H4.CarbonCount
+    ReadOnly Property CarbonCount As Integer Implements IC1H4.CarbonCount
             Return 1
     End Property
 
-    Public ReadOnly Property HydrogenCount As Integer Implements IC1H4.HydrogenCount
+    ReadOnly Property HydrogenCount As Integer Implements IC1H4.HydrogenCount
             Return 4
     End Property
 End Class
@@ -151,10 +151,10 @@ End Class
 Class C5H8
     Implements IC1H4
           
-    Public ReadOnly Property CarbonCount As Integer Implements IC1H4.CarbonCount
+    ReadOnly Property CarbonCount As Integer Implements IC1H4.CarbonCount
             Return 5
     End Property
-    Public ReadOnly Property HydrogenCount As Integer Implements IC1H4.HydrogenCount
+    ReadOnly Property HydrogenCount As Integer Implements IC1H4.HydrogenCount
             Return 8
     End Property
 End Class
@@ -162,10 +162,10 @@ End Class
 Class C13H24
     Implements IC1H4
           
-    Public ReadOnly Property CarbonCount As Integer Implements IC1H4.CarbonCount
+    ReadOnly Property CarbonCount As Integer Implements IC1H4.CarbonCount
             Return 13
     End Property
-    Public ReadOnly Property HydrogenCount As Integer Implements IC1H4.HydrogenCount
+    ReadOnly Property HydrogenCount As Integer Implements IC1H4.HydrogenCount
             Return 24
     End Property
 End Class
@@ -191,37 +191,37 @@ End Module
 Console.WriteLine(C1H4,C5H8,C13H24)
 
 Module Green
-Public Interface IC2H2
+Interface IC2H2
     ReadOnly Property CarbonCount As Integer
     ReadOnly Property HydrogenCount As Integer
 End Interface
 
 Class C2H2
     Implements IC2H2
-    Public ReadOnly Property CarbonCount As Integer Implements IC2H2.CarbonCount
+    ReadOnly Property CarbonCount As Integer Implements IC2H2.CarbonCount
             Return 2
     End Property
-    Public ReadOnly Property HydrogenCount As Integer Implements IC2H2.HydrogenCount
+    ReadOnly Property HydrogenCount As Integer Implements IC2H2.HydrogenCount
             Return 2
     End Property
 End Class
 
 Class C4H2
     Implements IC2H2
-    Public ReadOnly Property CarbonCount As Integer Implements IC2H2.CarbonCount
+    ReadOnly Property CarbonCount As Integer Implements IC2H2.CarbonCount
             Return 4
     End Property
-    Public ReadOnly Property HydrogenCount As Integer Implements IC2H2.HydrogenCount
+    ReadOnly Property HydrogenCount As Integer Implements IC2H2.HydrogenCount
             Return 2
     End Property
 End Class
 
 Class C6H2
     Implements IC2H2
-    Public ReadOnly Property CarbonCount As Integer Implements IC2H2.CarbonCount
+    ReadOnly Property CarbonCount As Integer Implements IC2H2.CarbonCount
             Return 6 
     End Property
-    Public ReadOnly Property HydrogenCount As Integer Implements IC2H2.HydrogenCount
+    ReadOnly Property HydrogenCount As Integer Implements IC2H2.HydrogenCount
             Return 2
     End Property
 End Class
@@ -248,37 +248,37 @@ End Module
 Console.WriteLine(C2H2,C4H2,C6H2)
 
 Module white
-Public Interface IC2H4
+Interface IC2H4
     ReadOnly Property CarbonCount As Integer
     ReadOnly Property HydrogenCount As Integer
 End Interface
 
 Class C2H4
     Implements IC2H4
-    Public ReadOnly Property CarbonCount As Integer Implements IC2H2.CarbonCount
+    ReadOnly Property CarbonCount As Integer Implements IC2H2.CarbonCount
             Return 2
     End Property
-    Public ReadOnly Property HydrogenCount As Integer Implements IC2H2.HydrogenCount
+    ReadOnly Property HydrogenCount As Integer Implements IC2H2.HydrogenCount
             Return 4
     End Property
 End Class
 
 Class C6H8
     Implements IC2H4
-    Public ReadOnly Property CarbonCount As Integer Implements IC2H2.CarbonCount
+    ReadOnly Property CarbonCount As Integer Implements IC2H2.CarbonCount
             Return 6
     End Property
-    Public ReadOnly Property HydrogenCount As Integer Implements IC2H2.HydrogenCount
+    ReadOnly Property HydrogenCount As Integer Implements IC2H2.HydrogenCount
             Return 8
     End Property
 End Class
 
 Class C14H12
     Implements IC2H4
-    Public ReadOnly Property CarbonCount As Integer Implements IC2H2.CarbonCount
+    ReadOnly Property CarbonCount As Integer Implements IC2H2.CarbonCount
             Return 14
     End Property
-    Public ReadOnly Property HydrogenCount As Integer Implements IC2H2.HydrogenCount
+    ReadOnly Property HydrogenCount As Integer Implements IC2H2.HydrogenCount
             Return 12
     End Property
 End Class
@@ -303,17 +303,17 @@ Console.WriteLine(C2H4,C6H8,C14H12)
 
 Module XYZT
 Class SpherePoint
-    char X = r * Math.sin(theta) * Math.cos(fai)
-    char Y = r * Math.sin(theta) * Math.sin(fai)
-    char Z = r * Math.cos(fai)
-    char T = r * Math.sin(theta)
+    char X = r * sin(theta) * cos(fai)
+    char Y = r * sin(theta) * sin(fai)
+    char Z = r * cos(fai)
+    char T = r * sin(theta)
     return "X,Y,Z,T"
 End Class
 
 Class SphereRadius
-    const SphereRadiusx = Math.sin(y) + Math.cos(z) 
-    const SphereRadiusy = Math.sin(x) + Math.cos(z) 
-    const SphereRadiusz = Math.sin(y) + Math.cos(x)
+    const SphereRadiusx = sin(y) + cos(z) 
+    const SphereRadiusy = sin(x) + cos(z) 
+    const SphereRadiusz = sin(y) + cos(x)
     return "SphereRadiusx, SphereRadiusy, SphereRadiusz"
 End Class
 End Module
@@ -328,5 +328,6 @@ const impact5 = "impact0 * impact1" + "impact2 * impact3"
 const impact6 = "impact1 - impact2" + "impact3 - impact4"
 const impact7 = "impact2 / impact3" + "impact4 / impact1"
 const impact8 = "impact3 % impact4" + "impact1 % impact2"
+
 
 Console.WriteLine(impact0, impact1, impact2, impact3, impact4, impact5, impact6, impact7, impact8)
