c996f69b4c7900c24818fb4d4f228718c9f13c73
unknownsaying <15368104568@189.cn>
2026-01-03 17:56:32 +0800

    Update StringCycle.vb

--- AI/String/StringCycle.vb
+++ AI/String/StringCycle.vb
@@ -1,30 +1,30 @@
-Public Class GuitarString
-    Public Property Note As String
-    Public Property Frequency As Double ' in Hz
-    Public Property StringColor As Color
-    Public Property YPosition As Integer
-    Public Property Length As Integer
-    Public Property Tension As Double
-    Public Property MassDensity As Double ' μ in physics equations
+Class GuitarString
+    Property Note As String
+    Property Frequency As Double ' in Hz
+    Property StringColor As Color
+    Property YPosition As Integer
+    Property Length As Integer
+    Property Tension As Double
+    Property MassDensity As Double ' μ in physics equations
     
     ' String vibration state
-    Public Property IsPlucked As Boolean = False
-    Public Property PluckTime As Double = 0
+    Property IsPlucked As Boolean = False
+    Property PluckTime As Double = 0
     
-    Public Overrides Function ToString() As String
+    Overrides Function ToString() As String
         Return $"{Note} String: f={Frequency}Hz, T={Tension}, μ={MassDensity}"
     End Function
 End Class
 
-Public Module StringPhysics
+Module StringPhysics
     ' Calculate wave speed on string
-    Public Function WaveSpeed(tension As Double, massDensity As Double) As Double
+    Function WaveSpeed(tension As Double, massDensity As Double) As Double
         ' v = √(T/μ)
         Return Math.Sqrt(tension / massDensity)
     End Function
 
     ' Calculate harmonic frequencies
-    Public Function HarmonicFrequencies(fundamental As Double, 
+    Function HarmonicFrequencies(fundamental As Double, 
                                         harmonicCount As Integer) As Double()
         Dim harmonics(harmonicCount - 1) As Double
         For i As Integer = 0 To harmonicCount - 1
@@ -34,7 +34,7 @@ Public Module StringPhysics
     End Function
 
     ' Superstring vibration modes (inspired by string theory)
-    Public Function SuperstringMode(n As Integer, 
+    Function SuperstringMode(n As Integer, 
                                     dimension As Integer, 
                                     tension As Double) As Double
         ' Simplified version of string theory vibration energy
@@ -42,7 +42,7 @@ Public Module StringPhysics
     End Function
 
     ' Calculate standing wave pattern
-    Public Function StandingWave(x As Double, 
+    Function StandingWave(x As Double, 
                                  t As Double, 
                                  frequency As Double, 
                                  length As Double, 
@@ -53,9 +53,6 @@ Public Module StringPhysics
     End Function
 End Module
 
-Imports System
-Imports System.Windows.Forms
-
 Module Program
     <STAThread>
     Sub Main()
@@ -63,4 +60,5 @@ Module Program
         Application.SetCompatibleTextRenderingDefault(False)
         Application.Run(New MainForm())
     End Sub
+
 End Module
