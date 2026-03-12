dba9f8dd44893b65f5fc59967cdc503d2e644e0b
unknownsaying <15368104568@189.cn>
2026-03-12 11:08:35 +0800

    Update Quark.vb

--- math/Quark.vb
+++ math/Quark.vb
@@ -4,10 +4,11 @@ const c^2 = 299792458*299792458
 name : {Bottom Charm Down Strange Top Up}
 mass : [4180 MeV/c^2 & 1275 MeV/c^2 & 4.8 MeV/c^2 & 96 MeV/c^2 & 172500 MeV/c^2 & 2.3 MeV/c^2]
 momentum : < & & & & & >
-charge : ( -1/3 | 2/3 | -1/3 | -1/3 | 2/3 | 2/3 )
+charge : ( -1/3e | +2/3e | -1/3e | -1/3e | +2/3e | +2/3e )
 mass-charge-scale : ( | | | | | )
 spin : [1/2 = 1/2 = 1/2 = 1/2 = 1/2 = 1/2 ]
 mass-spin-propability : [ = = = = = ]
 color : || red && green && blue || 
 
 End Enum
+
